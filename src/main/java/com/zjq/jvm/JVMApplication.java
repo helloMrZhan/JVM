7a5f2b439ae982f3f446c9031ddedce2159c5625
Jack <1187625098@qq.com>
2022-03-25 21:52:31 +0800

    JVM项目依赖初始化

---
+++ src/main/java/com/zjq/jvm/JVMApplication.java
@@ -0,0 +1,18 @@
+package com.zjq.jvm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * JVM从入门到精通
+ *
+ * @author zjq
+ */
+@SpringBootApplication
+public class JVMApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JVMApplication.class, args);
+    }
+
+}
