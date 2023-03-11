9fa35d8e6c838dead3ffb9cbfb77cd1ab32bc9fa
zjq <1187625098@qq.com>
2023-03-11 20:56:26 +0800

    String intern()方法测试

--- src/main/java/com/zjq/jvm/stringpool/TestIntern.java
+++ src/main/java/com/zjq/jvm/stringpool/TestIntern.java
@@ -13,12 +13,16 @@ public class TestIntern {
     @Test
     public void testIntern(){
         String c = "world";
+        // true
         System.out.println(c.intern() == c);
         String d = new String("mike");
+        // false
         System.out.println(d.intern() == d);
         String e = new String("jo") + new String("hn");
+        // true
         System.out.println(e.intern() == e);
         String f = new String("ja") + new String("va");
+        // false
         System.out.println(f.intern() == f);
     }
 }
